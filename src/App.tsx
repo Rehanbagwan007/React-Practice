369a97b90b3f4571b0f21f4f8a28aaf38cf65815
Rehanbagwan007 <rehanbagwan10000@gmail.com>
2025-10-02 04:28:59 +0000

    add

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useContext } from 'react'
+
+function App() {
+  const theme = useContext(ThemeContext)
+
+  return (
+    <>
+
+    <div className='bg-blue-800' >Hello</div>
+
+
+
+
+     
+    </>
+  )
+}
+
+export default App
